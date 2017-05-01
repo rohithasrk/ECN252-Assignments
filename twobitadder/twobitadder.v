ebb0eaf5c4f5fd012a094c1fcf9e8ab56d9b0c8e
Rohith Asrk <rohith.asrk@gmail.com>
2017-05-02 01:37:41 +0530

    Removed unecessary wire

--- twobitadder/twobitadder.v
+++ twobitadder/twobitadder.v
@@ -22,13 +22,13 @@ module twobitadder(
 	input [1:0] a,
 	input [1:0] b,
 	input cin,
-	output wire [1:0] sum,
-	output wire carry
+	output [1:0] sum,
+	output carry
 );
 
 wire c;
 
-fulladder f1(.a(a[0]), .b(b[0]), .cin(0), .sum(sum[0]), .carry(c));
+fulladder f1(.a(a[0]), .b(b[0]), .cin(0), .sum(sum	[0]), .carry(c));
 fulladder f2(.a(a[1]), .b(b[1]), .cin(c), .sum(sum[1]), .carry(carry));
 
 endmodule
